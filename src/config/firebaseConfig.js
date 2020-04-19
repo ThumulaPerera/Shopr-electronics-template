98e1ab68c221f47209e3cf24de5b0223fa31d779
Thumula <thumusteam@gmail.com>
2020-04-19 17:51:43 +0530

    changed the databsse

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,17 +3,29 @@ import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/firestore' // <- needed if using firestore
 
-//firebase config
-const fbConfig = {
-  apiKey: "AIzaSyCYnzL0jsdr0eErI74VedANNoseVfYsIkI",
-  authDomain: "electronics-template.firebaseapp.com",
-  databaseURL: "https://electronics-template.firebaseio.com",
-  projectId: "electronics-template",
-  storageBucket: "electronics-template.appspot.com",
-  messagingSenderId: "807802334387",
-  appId: "1:807802334387:web:49c900401304a6063294c6",
-  measurementId: "G-FH221XRGNM"
-}
+//firebase config for electronics-template
+// const fbConfig = {
+//   apiKey: "AIzaSyCYnzL0jsdr0eErI74VedANNoseVfYsIkI",
+//   authDomain: "electronics-template.firebaseapp.com",
+//   databaseURL: "https://electronics-template.firebaseio.com",
+//   projectId: "electronics-template",
+//   storageBucket: "electronics-template.appspot.com",
+//   messagingSenderId: "807802334387",
+//   appId: "1:807802334387:web:49c900401304a6063294c6",
+//   measurementId: "G-FH221XRGNM"
+// }
+
+//firebase config for ecom-cse
+var fbConfig = {
+  apiKey: "AIzaSyDgLsvomzUN0uBvH-QvBjfp_yvBPgeSuR8",
+  authDomain: "ecom-cse.firebaseapp.com",
+  databaseURL: "https://ecom-cse.firebaseio.com",
+  projectId: "ecom-cse",
+  storageBucket: "ecom-cse.appspot.com",
+  messagingSenderId: "535828749873",
+  appId: "1:535828749873:web:c991827f83d3ecef83bf10",
+  measurementId: "G-898N0JYJT9"
+};
 
 // react-redux-firebase config
 const rrfConfig = {
