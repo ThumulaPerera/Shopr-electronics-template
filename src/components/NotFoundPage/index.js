d885c05c4183e455c09faa39e7118db53072cd2f
Thumula <thumusteam@gmail.com>
2020-05-23 08:44:54 +0530

    not found page added

--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+  Segment, Image, Header, Divider,
+} from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+
+import { oopsImgUrl } from '../../constants/defaults';
 
 function NotFoundPage() {
   return (
-    <div>
-      <h>404 page not found</h>
-    </div>
+    <Segment basic textAlign="center" padded="very">
+      <Image src={oopsImgUrl} centered />
+      <Header size="huge">
+        404
+        <Header.Subheader>
+          Page not found
+        </Header.Subheader>
+      </Header>
+      <Divider hidden />
+      The page you are looking for does not exist
+      <br />
+      <NavLink to="/">
+        Go to home page
+      </NavLink>
+    </Segment>
   );
 }
 
--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
-export const defaultImgUrl = 'https://www.cowgirlcontractcleaning.com/wp-content/uploads/sites/360/2018/05/placeholder-img-1.jpg';
+export const defaultImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Fdefault%20images%2FdefaultItem.jpg?alt=media&token=25d58086-a46e-42dc-9228-82a3b1a116d9';
+export const oopsImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Fdefault%20images%2Foops.png?alt=media&token=247bd123-3e66-465f-babe-23a04eb8d1b0';
