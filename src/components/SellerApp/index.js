6e1c68d67b2b4d33022fa014ee6296be0f105a46
Thumula <thumusteam@gmail.com>
2020-05-24 22:58:05 +0530

    footer basic

--- src/components/SellerApp/index.js
+++ src/components/SellerApp/index.js
@@ -11,6 +11,7 @@ import PropTypes from 'prop-types';
 import { Loader } from 'semantic-ui-react';
 
 import NavBar from '../NavBar';
+import Footer from '../Footer';
 import BrowseProducts from '../BrowseProducts';
 import MyPurchases from '../MyPurchases';
 import Cart from '../Cart';
@@ -34,40 +35,43 @@ function SellerApp({ sellerStore }) {
 
   return (
     <div className="App" ref={contextRef}>
-      <NavBar contextRef={contextRef} />
-      <ReduxToastr
-        position="bottom-left"
-        transitionIn="fadeIn"
-        transitionOut="fadeOut"
-        timeOut={4000}
-        newestOnTop={false}
-      />
-      {
+      <div style={{ minHeight: '100vh' }}>
+        <NavBar contextRef={contextRef} />
+        <ReduxToastr
+          position="bottom-left"
+          transitionIn="fadeIn"
+          transitionOut="fadeOut"
+          timeOut={4000}
+          newestOnTop={false}
+        />
+        {
         sellerStore[0].enableChatbot
         && <ChatBot storeID={storeID} />
       }
-      <Switch>
-        <Route
-          path={`${path}${ROUTES.MY_PURCHASES_ROUTE}`}
-          component={MyPurchases}
-        />
-        <Route
-          path={`${path}${ROUTES.CART_ROUTE}`}
-          component={Cart}
-        />
-        <Route
-          path={`${path}${ROUTES.PROFILE_ROUTE}`}
-          component={Profile}
-        />
-        <Route
-          path={`${path}${ROUTES.ITEMS_ROUTE}/:itemId`}
-          component={ItemPage}
-        />
-        <Route
-          path={`${path}${ROUTES.HOME_ROUTE}`}
-          render={(props) => <BrowseProducts {...props} contextRef={contextRef} />}
-        />
-      </Switch>
+        <Switch>
+          <Route
+            path={`${path}${ROUTES.MY_PURCHASES_ROUTE}`}
+            component={MyPurchases}
+          />
+          <Route
+            path={`${path}${ROUTES.CART_ROUTE}`}
+            component={Cart}
+          />
+          <Route
+            path={`${path}${ROUTES.PROFILE_ROUTE}`}
+            component={Profile}
+          />
+          <Route
+            path={`${path}${ROUTES.ITEMS_ROUTE}/:itemId`}
+            component={ItemPage}
+          />
+          <Route
+            path={`${path}${ROUTES.HOME_ROUTE}`}
+            render={(props) => <BrowseProducts {...props} contextRef={contextRef} />}
+          />
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
