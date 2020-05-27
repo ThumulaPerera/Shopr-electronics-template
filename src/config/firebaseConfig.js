290935082ff6b2841fc016a56701b4a5d625bcca
Thumula <thumusteam@gmail.com>
2020-05-27 21:56:45 +0530

    enabled firestore persistence

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -40,5 +40,18 @@ firebase.initializeApp(fbConfig);
 firebase.firestore(); // <- needed if using firestore
 // firebase.functions() // <- needed if using httpsCallable
 
+firebase.firestore().enablePersistence()
+  .catch((err) => {
+    if (err.code === 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code === 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  });
+
 export default firebase;
 export { rrfConfig };
