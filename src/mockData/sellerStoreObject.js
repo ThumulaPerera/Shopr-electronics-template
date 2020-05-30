0e4f349f2517a5a5c25c66caa4aac77b913e16c4
Thumula <thumusteam@gmail.com>
2020-05-30 23:16:28 +0530

    mock data for testing

---
+++ src/mockData/sellerStoreObject.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+export const sellerStore = {
+  categories: [
+    {
+      name: 'category 0',
+    },
+    {
+      name: 'category 1',
+    },
+    {
+      name: 'category 2',
+    },
+    {
+      name: 'category 3',
+    },
+    {
+      name: 'category 4',
+    },
+  ],
+  currency: 'USD',
+  email: 'hendersonreed@orbiflex.com',
+  enableChatbot: true,
+  enableInventoryManagement: false,
+  enableRating: true,
+  verified: false,
+  storeName: 'ut qui',
+  storeCustomization: {},
+};
