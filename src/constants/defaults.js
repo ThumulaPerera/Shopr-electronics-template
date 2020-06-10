001174267419a1ba5eb9d697a92f86818143d9d3
Thumula <thumusteam@gmail.com>
2020-06-10 10:33:46 +0530

    urls changed

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export const defaultImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Fdefault%20images%2FdefaultItem.jpg?alt=media&token=25d58086-a46e-42dc-9228-82a3b1a116d9';
 
-export const browseProductsPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Fbrowse%20products%20page.jpg?alt=media&token=1b13816e-ff59-4482-8301-e30d52a89162';
-export const itemPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Fitem%20page.jpg?alt=media&token=54839d86-7da3-419d-a5a8-19343289051b';
-export const cartPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Fcart%20page.jpg?alt=media&token=7cd2aed4-52a6-42a1-a154-a078e3f4861b';
-export const myPurshasesPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Forders%20page.jpg?alt=media&token=7293f018-0fbd-4eb7-8c47-af0ded528358';
+export const browseProductsPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Fbrowse%20products%20page%20bordered.jpg?alt=media&token=fffe3e37-eb40-42b4-8203-b35a6b3d90b8';
+export const itemPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Fitem%20page%20bordered.jpg?alt=media&token=c3010e16-a691-45cf-96fc-a2f917614dbb';
+export const cartPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Fcart%20page%20bordered.jpg?alt=media&token=6a71097e-7d15-4eda-bb90-9d1b2a879e87';
+export const myPurshasesPageImgUrl = 'https://firebasestorage.googleapis.com/v0/b/ecom-cse.appspot.com/o/electronics-template%2Ftemplate%20screenshots%2Forders%20page%20borderd.jpg?alt=media&token=67e12d90-cc51-4f5c-8e93-3c9f3e561fdb';
